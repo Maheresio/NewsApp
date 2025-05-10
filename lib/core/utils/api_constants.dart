da7eff04b3fc5e331e73b9e800bf60c0b8e93b8f
Maheresio <zamalek.545r5@gmail.com>
2025-05-10 17:40:19 +0300

    add flutter_dotnev package and use it

--- lib/core/utils/api_constants.dart
+++ lib/core/utils/api_constants.dart
@@ -1,12 +1,14 @@
-abstract class ApiConstants {
-  static const String _apiKey = '708465389c8845688b057f35b7b519bc';
-  static const String _baseUrl = 'https://newsapi.org/v2';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-  static const String breakingNewsUrl =
+abstract class ApiConstants {
+  static String get _apiKey => dotenv.env['NEWS_API_KEY'] ?? '';
+  static String get _baseUrl => dotenv.env['NEWS_API_BASE_URL'] ?? '';
+
+  static String get breakingNewsUrl =>
       '$_baseUrl/top-headlines?country=us&apiKey=$_apiKey';
-  static const String recommendationUrl =
+  static String get recommendationUrl =>
       '$_baseUrl/everything?q=keyword&apiKey=$_apiKey';
 
-  static const String discoverNewsUrl =
+  static String get discoverNewsUrl =>
       '$_baseUrl/everything?domains=wsj.com&apiKey=$_apiKey';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -18,6 +19,7 @@ import 'repos/news_repo_impl.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   await ScreenUtil.ensureScreenSize();
   await clearCache();
   serviceLocator();
